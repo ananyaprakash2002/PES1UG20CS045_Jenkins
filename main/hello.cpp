6721416cb452ddacc6eebe30a648dda4e06fc774
ananyaprakash2002 <ananya.prakash2002@gmail.com>
2023-02-15 20:32:36 +0530

    update cpp file

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,8 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "Hello there Jenkins\n";
+  cout << " This is CC lab! \n";
   cout << "I have successfully built and run\n";
   return 0;
 }
